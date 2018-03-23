7512808aa5d6dc7a5da2de283c84f218cbcca805
bs666 <brservice@comcast.net>
2018-03-23 08:49:10 -0400

    activity three start

--- src/com/company/Shuffler.java
+++ src/com/company/Shuffler.java
@@ -51,6 +51,11 @@ public class Shuffler {
 	 */
 	public static void perfectShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		for(int j = 0; j <= 25; j++;)
+		{
+			
+		}
+
 	}
 
 	/**
